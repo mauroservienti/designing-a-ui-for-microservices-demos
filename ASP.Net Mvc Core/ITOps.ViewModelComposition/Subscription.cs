258a7863461000359489f1329e45c180ddc5480f
mauroservienti <mauro@topics.it>
2017-04-27 15:17:27 +0200

    Mvc Demo

---
+++ ASP.Net Mvc Core/ITOps.ViewModelComposition/Subscription.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using System.Threading.Tasks;
+
+namespace ITOps.ViewModelComposition
+{
+    abstract class Subscription
+    {
+        public abstract Task Invoke(dynamic viewModel, object @event, RouteData routeData, IQueryCollection query);
+    }
+}
