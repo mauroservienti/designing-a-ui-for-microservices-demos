f3fc61e8a0387b5852098046ebe72d22751f9d58
mauroservienti <mauro@topics.it>
2017-05-03 19:43:45 +0200

    split first demo in 2 solutions

---
+++ ASP.Net Core API Gateway - 01/ITOps.ViewModelComposition/IRouteInterceptor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using System.Threading.Tasks;
+
+namespace ITOps.ViewModelComposition
+{
+    public interface IRouteInterceptor
+    {
+        bool Matches(RouteData routeData, string httpVerb);
+    }
+
+    public interface IViewModelAppender : IRouteInterceptor
+    {
+        Task Append(dynamic vm, RouteData routeData, IQueryCollection query);
+    }
+}
