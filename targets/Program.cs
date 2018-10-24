91497b14330b3a68f84351f7b3ed146a01653b31
Mauro Servienti <mauro.servienti@gmail.com>
2018-10-24 19:33:42 +0200

    Add build support

--- targets/Program.cs
+++ targets/Program.cs
@@ -7,17 +7,27 @@ internal class Program
 {
     public static void Main(string[] args)
     {
-        Target("default", DependsOn("demos", "exercises"));
+        Target("default", DependsOn("Demo-01", "Demo-02", "Demo-03", "Demo-04"));
 
         Target(
-            "demos",
-            Directory.EnumerateFiles("demos", "*.sln", SearchOption.AllDirectories),
-            solution => Cmd("dotnet", $"build {solution} --configuration Debug"));
+            "Demo-01",
+            Directory.EnumerateFiles("ASP.Net Core API Gateway - 01", "*.sln", SearchOption.AllDirectories),
+            solution => Cmd("dotnet", $"build \"{solution}\" --configuration Debug"));
 
         Target(
-            "exercises",
-            Directory.EnumerateFiles("exercises", "*.sln", SearchOption.AllDirectories),
-            solution => Cmd("dotnet", $"build {solution} --configuration Debug"));
+            "Demo-02",
+            Directory.EnumerateFiles("ASP.Net Core API Gateway - 02", "*.sln", SearchOption.AllDirectories),
+            solution => Cmd("dotnet", $"build \"{solution}\" --configuration Debug"));
+
+        Target(
+            "Demo-03",
+            Directory.EnumerateFiles("ASP.Net Mvc Core", "*.sln", SearchOption.AllDirectories),
+            solution => Cmd("dotnet", $"build \"{solution}\" --configuration Debug"));
+
+        Target(
+            "Demo-04",
+            Directory.EnumerateFiles("ASP.Net Mvc Core UI Composition", "*.sln", SearchOption.AllDirectories),
+            solution => Cmd("dotnet", $"build \"{solution}\" --configuration Debug"));
 
         RunTargets(args);
     }
