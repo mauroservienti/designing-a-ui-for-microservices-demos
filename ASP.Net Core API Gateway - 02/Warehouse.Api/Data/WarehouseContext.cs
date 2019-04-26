7469dc806cabe8101838f802abc793169d854c2e
mauroservienti <mauro.servienti@gmail.com>
2019-04-26 15:56:22 +0200

    APIs (unchanged from Demo 1)

---
+++ ASP.Net Core API Gateway - 02/Warehouse.Api/Data/WarehouseContext.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Warehouse.Data.Models;
+
+namespace Warehouse.Data
+{
+    public class WarehouseContext : DbContext
+    {
+        public static void CreateSeedData()
+        {
+            using (var db = new WarehouseContext())
+            {
+                foreach (var stockItem in Initial.Data())
+                {
+                    db.StockItems.Add(stockItem);
+                }
+
+                db.SaveChanges();
+            }
+        }
+
+        public DbSet<StockItem> StockItems { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase(databaseName: "Warehouse");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StockItem>();
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        internal static class Initial
+        {
+            internal static StockItem[] Data()
+            {
+                return new[]
+                {
+                    new StockItem()
+                    {
+                        Id = 1,
+                        ProductId = 1,
+                        Inventory = 4,
+                    },
+                    new StockItem()
+                    {
+                        Id = 2,
+                        ProductId = 2,
+                        Inventory = 0,
+                    }
+                };
+            }
+
+        }
+    }
+}
