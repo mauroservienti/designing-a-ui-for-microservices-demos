bfab45bbcb7da8b249a4c2af1ddb015319b55261
Mauro Servienti <mauro.servienti@gmail.com>
2019-04-26 14:14:54 +0200

    Sales (single item composition support)

---
+++ ASP.Net Core API Gateway - 01/Sales.Api/Controllers/PricesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sales.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sales.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PricesController : ControllerBase
+    {
+        [HttpGet]
+        [Route("product/{id}")]
+        public dynamic Get(int id)
+        {
+            using (var db = new SalesContext())
+            {
+                var item = db.ProductsPrices
+                    .Where(o => o.Id == id)
+                    .SingleOrDefault();
+
+                return item;
+            }
+        }
+
+        [HttpGet]
+        [Route("products/{ids}")]
+        public IEnumerable<dynamic> Get(string ids)
+        {
+            using (var db = new SalesContext())
+            {
+                var productIds = ids.Split(",".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).Select(s => int.Parse(s)).ToArray();
+                var items = db.ProductsPrices
+                    .Where(status => productIds.Any(id => id == status.Id))
+                    .ToArray();
+
+                return items;
+            }
+        }
+    }
+}
