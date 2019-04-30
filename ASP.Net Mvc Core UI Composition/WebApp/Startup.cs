f128d30c9a2dd1b6d963021042aeecf96abc0a35
Mauro Servienti <mauro.servienti@gmail.com>
2019-04-30 15:33:54 +0200

    New UI based on View Components

---
+++ ASP.Net Mvc Core UI Composition/WebApp/Startup.cs
@@ -0,0 +1,38 @@
+﻿using ServiceComposer.AspNetCore;
+using ServiceComposer.AspNetCore.Mvc;
+using ServiceComposer.AspNetCore.Gateway;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using ITOps.UIComposition.Mvc;
+
+namespace WebApp
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().AddUIComposition();
+            services.AddViewModelComposition(options=>
+            {
+                options.AddMvcSupport();
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseBrowserLink();
+
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
