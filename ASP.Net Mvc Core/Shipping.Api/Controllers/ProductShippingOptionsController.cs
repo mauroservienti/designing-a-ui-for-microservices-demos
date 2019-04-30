bd851cf5bfabd4d560d00aae67d1ea87d8fb2556
Mauro Servienti <mauro.servienti@gmail.com>
2019-04-30 09:33:58 +0200

    Add composition support from previous demo

---
+++ ASP.Net Mvc Core/Shipping.Api/Controllers/ProductShippingOptionsController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shipping.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Shipping.Api.Controllers
+{
+    [Route("api/shipping-options")]
+    [ApiController]
+    public class ProductShippingOptionsController : ControllerBase
+    {
+        [HttpGet]
+        [Route("product/{id}")]
+        public dynamic Get(int id)
+        {
+            using (var db = new ShippingContext())
+            {
+                var item = db.ProductShippingOptions
+                    .Include(pso => pso.Options)
+                    .Where(o => o.ProductId == id)
+                    .SingleOrDefault();
+
+                return item;
+            }
+        }
+
+        [HttpGet]
+        [Route("products/{ids}")]
+        public IEnumerable<dynamic> Get(string ids)
+        {
+            using (var db = new ShippingContext())
+            {
+                var productIds = ids.Split(",".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).Select(s => int.Parse(s)).ToArray();
+                var items = db.ProductShippingOptions
+                    .Include(pso => pso.Options)
+                    .Where(status => productIds.Any(id => id == status.ProductId))
+                    .ToArray();
+
+                return items;
+            }
+        }
+    }
+}
