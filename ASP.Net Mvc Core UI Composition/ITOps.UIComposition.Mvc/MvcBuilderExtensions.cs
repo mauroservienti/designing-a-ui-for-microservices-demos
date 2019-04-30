9a483aa7b53529b888680957d05bd83f2c6fd469
Mauro Servienti <mauro.servienti@gmail.com>
2019-04-30 15:33:13 +0200

    UI Composition infrastructure

---
+++ ASP.Net Mvc Core UI Composition/ITOps.UIComposition.Mvc/MvcBuilderExtensions.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace ITOps.UIComposition.Mvc
+{
+    public static class MvcBuilderExtensions
+    {
+        public static IMvcBuilder AddUIComposition(this IMvcBuilder builder, string assemblySearchPattern = "*ViewComponents*.dll")
+        {
+            var fileNames = Directory.GetFiles(AppContext.BaseDirectory, assemblySearchPattern);
+
+            var assemblies = new List<(string BaseNamespace, Assembly Assembly)>();
+
+            foreach (var fileName in fileNames)
+            {
+                var assembly = Assembly.LoadFrom(fileName);
+                var attribute = assembly.GetCustomAttribute<UICompositionSupportAttribute>();
+
+                if (attribute != null)
+                {
+                    assemblies.Add((attribute.BaseNamespace, assembly));
+                }
+            }
+
+            assemblies.ForEach(a =>
+            {
+                builder.Services.Configure<RazorViewEngineOptions>(options =>
+                {
+                    options.FileProviders.Add(new EmbeddedFileProvider(a.Assembly, a.BaseNamespace));
+                });
+                builder.AddApplicationPart(a.Assembly);
+            });
+
+            return builder;
+        }
+    }
+}
---
+++ ASP.Net Mvc Core UI Composition/ITOps.UIComposition.Mvc/UICompositionSupportAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ITOps.UIComposition.Mvc
+{
+    public sealed class UICompositionSupportAttribute : Attribute
+    {
+        public UICompositionSupportAttribute(string baseNamespace)
+        {
+            this.BaseNamespace = baseNamespace;
+        }
+
+        public string BaseNamespace { get; private set; }
+    }
+}
