bcb696cc5f3aa7af5e9b58f13795fdabdf50b982
mauroservienti <mauro.servienti@gmail.com>
2020-06-19 14:43:23 +0200

    Simpler configuration for Demo 1

--- ASP.Net Core API Gateway - 01/Shipping.ViewModelComposition/ViewModelCompositionOptionsCustomization.cs
+++ ASP.Net Core API Gateway - 01/Shipping.ViewModelComposition/ViewModelCompositionOptionsCustomization.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ConfigurationUtils;
 using Microsoft.Extensions.DependencyInjection;
 using ServiceComposer.AspNetCore;
 
@@ -8,13 +9,7 @@ namespace Shipping.ViewModelComposition
     {
         public void Customize(ViewModelCompositionOptions options)
         {
-            options.AddServicesConfigurationHandler(typeof(ProductDetailsGetHandler), (type, services) =>
-            {
-                services.AddHttpClient<ProductDetailsGetHandler>(typeof(ProductDetailsGetHandler).FullName, client =>
-                {
-                    client.BaseAddress = new Uri("http://localhost:5004");
-                });
-            });
+            options.RegisterHttpClient<ProductDetailsGetHandler>("http://localhost:5004");
         }
     }
 }
