b76de525e3f8545dd4beab8d02ea6ee085ba5f84
mauroservienti <mauro.servienti@gmail.com>
2020-06-29 11:21:34 +0200

    Stop using shared projects

---
+++ ASP.Net Core API Gateway - 02/ConfigurationUtils/ViewModelCompositionOptionsExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ServiceComposer.AspNetCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ConfigurationUtils
+{
+    public static class ViewModelCompositionOptionsExtensions
+    {
+        public static void RegisterHttpClient<T>(this ViewModelCompositionOptions options, string baseAddress) where T: class
+        {
+            options.AddServicesConfigurationHandler(typeof(T), (type, services) =>
+            {
+                services.AddHttpClient<T>(typeof(T).FullName,
+                    client => client.BaseAddress = new Uri(baseAddress));
+            });
+        }
+    }
+}
